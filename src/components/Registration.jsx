43646705fe3654fdca3b99f33e5fa23c7001e513
COR Coding <corcoding@DESKTOP-QMOVT9L.localdomain>
2023-10-25 11:51:33 -0400

    saving changes for merge

--- src/components/Registration.jsx
+++ src/components/Registration.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 const Registration = () => {
   return (
     <div>
+      <form>
+        <fieldset>
 
+        <label for='email'>Email</label>
+        <input type='text' name='email'></input>
+
+        <label for='password'>Password</label>
+        <input type='password' name='password'></input>
+
+        <label for='password2'>Re-Enter Password</label>
+        <input type='password' name='password2' min='8' ></input>
+
+        </fieldset>
+
+        <button type='submit'>Submit</button>
+
+      </form>
     </div>
   )
 }
