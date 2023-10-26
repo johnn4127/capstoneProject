d8a142c11a70c9e1d840e7e2c3aa1bd77debdf8d
Hien Nguyen <140361387+hiennguyen727@users.noreply.github.com>
2023-10-26 10:55:24 -0400

    Backend (#11)
    * doing users backend
    
    * doing backend for users

---
+++ server/config/config.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+
+module.exports = {
+  "development": {
+    "username": "ograyfbl",
+    "password": process.env.DB_PASSWORD,
+    "database": "ograyfbl",
+    "host": "suleiman.db.elephantsql.com",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
