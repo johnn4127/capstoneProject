2e860fed3b099bf2b3394875a3caea3fdae85e4d
COR Coding <corcoding@DESKTOP-QMOVT9L.localdomain>
2023-10-26 10:42:32 -0400

    Creates registration page layout.

--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -1,25 +1,34 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import '../stylesheets/Registration.css'
 
 const Registration = () => {
   return (
     <div id='registration'>
-      <form>
-        <fieldset>
+      <Form>
+      <h1>Registration</h1>
+      
+        <Form.Group className='formField'>
+          <Form.Label for='email'>Email</Form.Label>
+          <Form.Control type='text' name='email' placeholder='Enter email address...'></Form.Control>
+        </Form.Group>
 
-        <label for='email'>Email</label>
-        <input type='text' name='email'></input>
+        <Form.Group className='formField'>
+          <Form.Label for='password'>Password</Form.Label>
+          <Form.Control type='password' name='password' placeholder='Enter password...'></Form.Control>
+        </Form.Group>
 
-        <label for='password'>Password</label>
-        <input type='password' name='password'></input>
+        <Form.Group className='formField'>
+          <Form.Label for='password2'>Re-Enter Password</Form.Label>
+          <Form.Control type='password' name='password2' placeholder='Re-enter password...' min='8' ></Form.Control>
+        </Form.Group>
+      
 
-        <label for='password2'>Re-Enter Password</label>
-        <input type='password' name='password2' min='8' ></input>
 
-        </fieldset>
+        <Button variant='primary' type='submit'>Submit</Button>
 
-        <button type='submit'>Submit</button>
-
-      </form>
+      </Form>
     </div>
   )
 }
