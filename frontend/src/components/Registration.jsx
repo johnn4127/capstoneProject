4ccb92d025c816db1134d07cd062168166d17b6e
hiennguyen727 <hien.d.nguyen1997@gmail.com>
2023-10-27 09:09:50 -0400

    reset point

--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -11,17 +11,19 @@ const Registration = () => {
   };
   return (
     <div id='registration'>
-      <Form>
+      <Form onSubmit={handleFormSubmit}>
       <h1>Registration</h1>
       
         <Form.Group className='formField'>
           <Form.Label for='email'>Email</Form.Label>
-          <Form.Control type='text' name='email' placeholder='Enter email address...'></Form.Control>
+          <Form.Control value={email}
+            onChange={(e) => setName(e.target.value)} type='text' name='email' placeholder='Enter email address...'></Form.Control>
         </Form.Group>
 
         <Form.Group className='formField'>
           <Form.Label for='password'>Password</Form.Label>
-          <Form.Control type='password' name='password' placeholder='Enter password...'></Form.Control>
+          <Form.Control value={password}
+            onChange={(e) => setName(e.target.value)} type='password' name='password' placeholder='Enter password...'></Form.Control>
         </Form.Group>
 
         <Form.Group className='formField'>
