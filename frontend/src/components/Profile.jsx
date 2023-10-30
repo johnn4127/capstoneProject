1d40e903abd37c1289241fcbaf4cce69a498b75f
Hien Nguyen <140361387+hiennguyen727@users.noreply.github.com>
2023-10-30 14:57:48 -0400

    some more profile/register/login (#38)

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,76 +1,51 @@
-import React, {useState,useEffect} from 'react'
-import { Button,Form,Container, Card, ListGroup } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Button, Form, Container, Card, ListGroup, Nav } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 const Profile = () => {
-  const [users, setUsers] = useState('');
-  const [formData, setFormData] = useState({
-    characterName: '',
-    confidence: '',
-  });
+  const [charName, setCharName] = useState(""); // Change variable name to charName to store character name
 
   const fetchUsersData = async () => {
-    const response = await fetch('http://localhost:3000/users', {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    setUsers(response)
+    try {
+      const response = await fetch(`http://localhost:3000/users:${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        const hello=JSON.stringify(data)
+        console.log(hello)
+        console.log('wahlfaksdjfklsjda')
+        setCharName(hello); // Set the character name in the state
+      } else {
+        console.error("Failed to fetch user data");
+      }
+    } catch (error) {
+      console.error("An error occurred while fetching user data:", error);
+    }
   };
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-  const handlesave = async () => {
-    const response = await fetch('http://localhost:3000/profile', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(formData)
-    })};
 
   useEffect(() => {
-    fetchUsersData(); // Call the fetch function
+    fetchUsersData();
   }, []);
+
   return (
+    <>
+      <h2>CHARACTERS</h2>
 
-   
-      <Container id='hello'>
-      <h2>Users:{users.id}</h2>
-      
-      <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
-            <Form.Label>Character Name</Form.Label>
-            <Form.Control
-              type="name"
-              placeholder="Enter character name"
-              name="characterName"
-              value={formData.characterName}
-              onChange={handleInputChange}
-            />
-          </Form.Group>
+      <Form.Label>{charName}</Form.Label> 
+      <Form.Label>PROFICIENCY:</Form.Label> 
+      <Form.Label>{charName}</Form.Label> 
+      <Form.Label>{charName}</Form.Label> 
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
-            <Form.Label>Confidence</Form.Label>
-            <Form.Control
-              type="number"
-              placeholder="Confidence"
-              name="confidence"
-              value={formData.confidence}
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-
-          <Button onClick={handlesave} variant="primary" type="button">
-            Save Character
-          </Button>
-        </Form>
-    </Container>
+      <Nav.Link className="navButton" as={Link} to="/game">
+        Start Game
+      </Nav.Link>
+    </>
   );
 };
 
-   
-
- 
-
-export default Profile
+export default Profile;
