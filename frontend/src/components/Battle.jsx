c8cf70ff1200a8ba801665629d8ba52d6950e981
johnn4127 <johnn4127@gmail.com>
2023-10-30 22:55:28 -0400

    adds confidence bar

--- frontend/src/components/Battle.jsx
+++ frontend/src/components/Battle.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
 import { useCharacter } from './CharacterContext';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 import '../stylesheets/Battle.css';
 
 const Battle = () => {
-  const [target1Charisma, setTarget1Charisma] = useState(100);
-  const [target2Charisma, setTarget2Charisma] = useState(100);
-  const [damageLogs, setDamageLogs] = useState([]);
+  const { charName } = useCharacter();
+  const [userCharacterConfidence, setUserCharacterConfidence] = useState(100);
+  const [enemyConfidence, setEnemyConfidence] = useState(100);
+  const [damageLog, setDamageLog] = useState('');
   const [enemyMessage, setEnemyMessage] = useState('');
   const [enemyLines, setEnemyLines] = useState([
     "Name a time you failed in life!",
     "Prepare to be coded out!",
     "I'm the master coder!",
     "Tell me about your salary expectations.",
-    "What are some of your weaknesses",
+    "What are some of your weaknesses"
   ]);
-
-  const { charName } = useCharacter();
 
   const getRandomEnemyLine = () => {
     const randomIndex = Math.floor(Math.random() * enemyLines.length);
@@ -23,28 +23,25 @@ const Battle = () => {
   };
 
   const handleAttack = () => {
-    const charismaDamage = Math.floor(Math.random() * (15 - 5 + 1) + 5);
+    const damage = Math.floor(Math.random() * (15 - 5 + 1) + 5);
 
     // Your attack
-    setTarget2Charisma(target2Charisma - charismaDamage);
+    setUserCharacterConfidence(userCharacterConfidence - damage);
 
-    // Add the damage log to the logs state
-    setDamageLogs(prevLogs => [
-      ...prevLogs,
-      `You attacked Coding Enemy for ${charismaDamage} charisma damage.`,
-    ]);
+    // Update the damage log with the most recent damage
+    setDamageLog(`You attacked Coding Enemy for ${damage} confidence damage.`);
 
     // Enemy's attack
-    const enemyCharismaDamage = Math.floor(Math.random() * (15 - 5 + 1) + 5);
-    setTarget1Charisma(target1Charisma - enemyCharismaDamage);
+    const enemyDamage = Math.floor(Math.random() * (15 - 5 + 1) + 5);
+    setEnemyConfidence(enemyConfidence - enemyDamage);
 
     const enemyLine = getRandomEnemyLine();
     setEnemyMessage(`Coding Enemy: "${enemyLine}"`);
 
-    // Add the enemy message and damage log to the logs state
-    setDamageLogs(prevLogs => [
-      ...prevLogs,
-      `Coding Enemy attacked you for ${enemyCharismaDamage} charisma damage.`,
+    // Add the enemy message to the damage log
+    setDamageLog(prevDamageLog => [
+      `${prevDamageLog}`,
+      `Coding Enemy attacked you for ${enemyDamage} confidence damage.`,
     ]);
   };
 
@@ -54,11 +51,11 @@ const Battle = () => {
       <div className="box">
         <div className="target-box">
           <h2>{charName}</h2>
-          <p>Charisma: {target1Charisma}</p>
+          <ProgressBar now={userCharacterConfidence} label={`Confidence: ${userCharacterConfidence}`} max={100} variant="success" />
         </div>
         <div className="target-box">
           <h2>CODING ENEMY</h2>
-          <p>Charisma: {target2Charisma}</p>
+          <ProgressBar now={enemyConfidence} label={`Confidence: ${enemyConfidence}`} max={100} variant="danger" />
         </div>
       </div>
       <button onClick={handleAttack}>Attack</button>
@@ -67,9 +64,7 @@ const Battle = () => {
         {enemyMessage && <p>{enemyMessage}</p>}
       </div>
       <div className="damage-info-box">
-        {damageLogs.map((log, index) => (
-          <p key={index}>{log}</p>
-        ))}
+        {damageLog && <p>{damageLog}</p>}
       </div>
     </div>
   );
