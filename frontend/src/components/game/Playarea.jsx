5cecaf68c40ca794e347a6831f5656189744648c
Dujuanye' Henry <139405590+dhenry827@users.noreply.github.com>
2023-10-30 23:00:54 -0400

    Adds playerContext to Game component and it's children, then adds conditional rendering to Playarea component for battle and shop components. (#44)
    Co-authored-by: COR Coding <corcoding@coding-14.attlocal.net>

--- frontend/src/components/game/Playarea.jsx
+++ frontend/src/components/game/Playarea.jsx
@@ -1,45 +1,56 @@
-import React, { useState, createContext, useEffect} from 'react'
+import React, { useState, createContext, useContext, useEffect } from 'react'
 import Background from './Background'
 import Player from './Player'
 import '../../stylesheets/Game.css'
 import Enemy from './Enemy'
 import Boss from './Boss'
+import { PlayerData } from './Game'
+import Shop from './Shop.jsx'
 
-export const  PositionData = createContext()
+export const PositionData = createContext()
 
 const Playarea = () => {
-const [playerPosition, setPlayerPosition] = useState({ x: 0, y: -350, width: 100 }) //sets initial position of player avatar
-const [enemyPosition, setEnemyPosition] = useState({ x: 400, y: 120, width: 100 }) //sets initial position of player avatar
-const [bossPosition, setBossPosition] = useState({ x: 1200, y: -600, width: 300 }) //sets initial position of player avatar
 
-const [battle, setBattle] = useState(false)
+  const { player, setPlayer, battle, setBattle, shop, setShop } = useContext(PlayerData)
 
-const checkCollision = () => {
-  const playerLeft = playerPosition.x;
-  const playerRight = playerPosition.x + playerPosition.width;
+  const [playerPosition, setPlayerPosition] = useState({ x: 0, y: -350, width: 100 }) //sets initial position of player avatar
+  const [enemyPosition, setEnemyPosition] = useState({ x: 400, y: 120, width: 100 }) //sets initial position of player avatar
+  const [bossPosition, setBossPosition] = useState({ x: 1200, y: -600, width: 300 }) //sets initial position of player avatar
+
   
 
-  const enemyLeft = enemyPosition.x;
-  const enemyRight = enemyPosition.x + enemyPosition.width;
+  const checkCollision = () => {
+    const playerLeft = playerPosition.x; //sets the left side of the player div
+    const playerRight = playerPosition.x + playerPosition.width; //sets the right side of the player div
 
-  const bossLeft = bossPosition.x;
-  const bossRight = bossPosition.x + bossPosition.width;
-  
-  if(
-    playerRight > enemyLeft && 
-    playerLeft < enemyRight 
-  ) {
-    console.log('Collision detected')
-  } 
-}
 
-useEffect(() => {
-  document.addEventListener('keydown', (event) => {
+    const enemyLeft = enemyPosition.x; //sets the left side of the enemy div
+    const enemyRight = enemyPosition.x + enemyPosition.width; //sets the right side of the enemy div
 
-    checkCollision();
-  })
+    const bossLeft = bossPosition.x; //sets the left side of the boss div
+    const bossRight = bossPosition.x + bossPosition.width; //sets the right side of the boss div
 
-},[playerPosition, enemyPosition])
+    if ( //checks to see if divs intersect
+      playerRight > enemyLeft &&
+      playerLeft < enemyRight
+    ) {
+      console.log('Collision detected')
+      setBattle(true)
+    }
+  }
+
+  const endBattle = () => { //used to undrender battle component and render shop componnent. Should later be moved to battle component during testing.
+    setBattle(false)
+    setShop(true)
+  }
+
+  useEffect(() => {
+    document.addEventListener('keydown', (event) => {
+
+      checkCollision();
+    })
+
+  }, [playerPosition, enemyPosition])
 
 
   return (
@@ -53,20 +64,29 @@ useEffect(() => {
           overflow: 'hidden',
           backgroundColor: 'darkgray'
         }}>
-        <PositionData.Provider value={{playerPosition, setPlayerPosition, enemyPosition, setEnemyPosition, bossPosition, setBossPosition}}>
-        {!battle ? (
-          <>
-          <Background />
-          <Player />
-          <Boss />
-          <Enemy />
-          </>
-          ) : (
+        <PositionData.Provider value={{ playerPosition, setPlayerPosition, enemyPosition, setEnemyPosition, bossPosition, setBossPosition }}>
+          {!battle && !shop ? ( //if shop and battle are false render this
             <>
-            {/* add battle component here */}
+              <Background />
+              <Player />
+              <Boss />
+              <Enemy />
             </>
-          )
-        }
+          ) : null}
+
+          {battle ? ( //if battle is true render this
+            <>
+              <button onClick={() => endBattle()}>End Battle</button>
+            </>
+          ) : null
+          }
+
+          {shop ? ( //if shop is true render this
+            <>
+              <Shop />
+            </>
+          ) : null}
+
         </PositionData.Provider>
       </div>
     </div>
