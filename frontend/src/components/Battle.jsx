2ac3d6ed4201c1f21c7c6f470aee6217a905ace8
hiennguyen727 <hien.d.nguyen1997@gmail.com>
2023-10-30 21:47:18 -0400

    did more functionality for battle and added some CSS

--- frontend/src/components/Battle.jsx
+++ frontend/src/components/Battle.jsx
@@ -1,44 +1,76 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { useCharacter } from './CharacterContext';
+import '../stylesheets/Battle.css';
 
 const Battle = () => {
   const [target1Charisma, setTarget1Charisma] = useState(100);
   const [target2Charisma, setTarget2Charisma] = useState(100);
-  const [damageInfo, setDamageInfo] = useState('');
-  const { charName } = useCharacter(); 
+  const [damageLogs, setDamageLogs] = useState([]);
+  const [enemyMessage, setEnemyMessage] = useState('');
+  const [enemyLines, setEnemyLines] = useState([
+    "Name a time you failed in life!",
+    "Prepare to be coded out!",
+    "I'm the master coder!",
+    "Tell me about your salary expectations.",
+    "What are some of your weaknesses",
+  ]);
+
+  const { charName } = useCharacter();
+
+  const getRandomEnemyLine = () => {
+    const randomIndex = Math.floor(Math.random() * enemyLines.length);
+    return enemyLines[randomIndex];
+  };
 
   const handleAttack = () => {
     const charismaDamage = Math.floor(Math.random() * (15 - 5 + 1) + 5);
 
     // Your attack
     setTarget2Charisma(target2Charisma - charismaDamage);
-    setDamageInfo(`You attacked Coding Enemy for ${charismaDamage} charisma damage.`);
+
+    // Add the damage log to the logs state
+    setDamageLogs(prevLogs => [
+      ...prevLogs,
+      `You attacked Coding Enemy for ${charismaDamage} charisma damage.`,
+    ]);
 
     // Enemy's attack
     const enemyCharismaDamage = Math.floor(Math.random() * (15 - 5 + 1) + 5);
     setTarget1Charisma(target1Charisma - enemyCharismaDamage);
-    setDamageInfo(prevDamageInfo => `${prevDamageInfo}\nCoding Enemy attacked you for ${enemyCharismaDamage} charisma damage.`);
+
+    const enemyLine = getRandomEnemyLine();
+    setEnemyMessage(`Coding Enemy: "${enemyLine}"`);
+
+    // Add the enemy message and damage log to the logs state
+    setDamageLogs(prevLogs => [
+      ...prevLogs,
+      `Coding Enemy attacked you for ${enemyCharismaDamage} charisma damage.`,
+    ]);
   };
 
   return (
     <div>
       <h1>CODING BATTLE</h1>
-      <div className="target">
-        <h2>{charName}</h2>
-        <p>Charisma: {target1Charisma}</p>
-      </div>
-      <div className="target">
-        <h2>CODING ENEMY</h2>
-        <p>Charisma: {target2Charisma}</p>
+      <div className="box">
+        <div className="target-box">
+          <h2>{charName}</h2>
+          <p>Charisma: {target1Charisma}</p>
+        </div>
+        <div className="target-box">
+          <h2>CODING ENEMY</h2>
+          <p>Charisma: {target2Charisma}</p>
+        </div>
       </div>
       <button onClick={handleAttack}>Attack</button>
       <p>It's your turn to attack</p>
-
-      {damageInfo && (
-        <div className="damage-info-box">
-          <p>{damageInfo}</p>
-        </div>
-      )}
+      <div className="enemy-message-box">
+        {enemyMessage && <p>{enemyMessage}</p>}
+      </div>
+      <div className="damage-info-box">
+        {damageLogs.map((log, index) => (
+          <p key={index}>{log}</p>
+        ))}
+      </div>
     </div>
   );
 };
