00a522389342421d9c458699b094381d4fad00a5
COR Coding <corcoding@coding-14.attlocal.net>
2023-10-30 22:56:10 -0400

    Adds playerContext to Game component and it's children, then adds conditional rendering to Playarea component for battle and shop components.

--- frontend/src/components/game/Game.jsx
+++ frontend/src/components/game/Game.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React, { useState, createContext } from 'react'
 import Playarea from './Playarea'
 
 
+export const PlayerData = createContext()
+
 const Game = () => {
+
+  const [player, setPlayer] = useState({proficiency: 10, confidence: 10, skills: [], exp: 1000})
+  const [battle, setBattle] = useState(false) //controls rendering of battle component
+  const [shop, setShop] = useState(false) //controls rendering of shop component
+  
   return (
-    <>
+    <> 
+    <PlayerData.Provider value={{player, setPlayer, battle, setBattle, shop, setShop}} > 
        <Playarea />
+    </PlayerData.Provider>
     </>
   )
 }
