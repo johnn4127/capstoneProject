8101da3a73a70f271d7417d16307cb06f95af339
John Nguyen <johnn4127@gmail.com>
2023-11-01 10:00:54 -0400

    Loginfunctionality (#60)
    * adds login alert
    
    * adds no user found
    
    * adds user not found, and incorrect email/password

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,8 +1,7 @@
-
-
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert'; 
 import Profile from './Profile';
 import { useNavigate } from 'react-router-dom';
 
@@ -13,6 +12,7 @@ const Login = () => {
   });
 
   const [loggedIn, setLoggedIn] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(''); 
   const navigate = useNavigate();
 
   const handleInputChange = (e) => {
@@ -22,7 +22,6 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      // CHANGE THIS ONCE YOU'VE DEPLOYED THE SERVER HEREHEHREHRHERHERHEHREHRE
       const response = await fetch('http://localhost:3000/login', {
         method: 'POST',
         headers: {
@@ -32,28 +31,29 @@ const Login = () => {
       });
 
       if (response.status === 200) {
-        
         setLoggedIn(true);
+        navigate('/profile');
+      } else if (response.status === 401) {
+        const data = await response.json(); 
 
-      
-        navigate('/profile'); 
-      } else {
-        
-        console.log('Login failed');
+        if (data.message === 'Unknown email') {
+          setErrorMessage('No user found with this email'); 
+        } else {
+          setErrorMessage('Invalid email or password');
+        }
       }
     } catch (error) {
-      
       console.error('Error during login:', error);
+      setErrorMessage('An error occurred during login');
     }
   };
 
   return (
-    <div>
-      {loggedIn ? (
-        <Profile />
-      ) : (
+    <div className="d-flex justify-content-center align-items-start" style={{ minHeight: '100vh' }}>
+      <div className="border rounded p-4" style={{ marginTop: '100px' }}>
+        <h1 className="text-center">Login</h1>
         <Form>
-          <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Group controlId="formBasicEmail">
             <Form.Label>Email</Form.Label>
             <Form.Control
               type="email"
@@ -64,7 +64,7 @@ const Login = () => {
             />
           </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control
               type="password"
@@ -75,11 +75,18 @@ const Login = () => {
             />
           </Form.Group>
 
-          <Button onClick={handleLogin} variant="primary" type="button">
-            Submit
-          </Button>
+          <div style={{ textAlign: 'center', margin: '10px' }}>
+            <Button onClick={handleLogin} variant="primary" type="button">
+              Submit
+            </Button>
+          </div>
+          {errorMessage && (
+            <Alert variant="danger">
+              {errorMessage}
+            </Alert>
+          )}
         </Form>
-      )}
+      </div>
     </div>
   );
 };
--- server/index.js
+++ server/index.js
@@ -24,7 +24,6 @@ app.post('/register', async (req, res) => {
   try {
     const { email, password, charName } = req.body;
 
-  
     const existingUser = await Users.findOne({
       where: {
         email: email,
@@ -32,18 +31,17 @@ app.post('/register', async (req, res) => {
     });
 
     if (existingUser) {
-
       res.status(409).json({ error: 'Email is already taken' });
       return;
     }
 
-  
     const hashedPassword = await bcrypt.hash(password, saltRounds);
 
     const newUser = await Users.create({
       email: email,
       password: hashedPassword,
       charName: charName,
+      
     });
 
     res.json(newUser);
@@ -93,20 +91,21 @@ app.post('/login', async (req, res) => {
     });
 
     if (!user) {
-      res.status(401).json('Invalid login credentials');
+    
+      res.status(401).json({ message: 'Unknown email' });
       return;
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (passwordMatch) {
-      res.json('Authentication successful');
+      res.json({ message: 'Authentication successful' });
     } else {
-      res.status(401).json('Invalid login credentials');
+      res.status(401).json({ message: 'Invalid login credentials' });
     }
   } catch (error) {
     console.error('Error during login:', error);
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ message: 'Internal server error' });
   }
 });
 
