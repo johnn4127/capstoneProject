646b47d6c8782cb82275240dff075a046ff35a56
hiennguyen727 <hien.d.nguyen1997@gmail.com>
2023-11-01 11:14:47 -0400

    did homepage and got rid of navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,15 +8,18 @@ import Game from './components/game/Game'
 import Shop from './components/game/Shop'
 import Actionbar from './components/Actionbar'
 import Battle from './components/Battle'
+import Char1 from './components/Char1'
+import Homepage from './components/Homepage'
 const App = () => {
   return (
     <div>
-      <Navbars/>
+      {/* <Navbars/> */}
 
 
       <Routes>
 
-      <Route path='/' element= {<Login/>}/>
+      <Route path='/' element= {<Homepage/>}/>
+      <Route path='*' element= {<Homepage/>}/>
       <Route path='/registration' element= {<Registration/>}/>
       <Route path='/profile' element= {<Profile/>}/>
       <Route path='/game' element= {<Game />}/>
@@ -24,6 +27,8 @@ const App = () => {
       <Route path='/actionbar' element= {<Actionbar/>}/>
       <Route path='/profile' element={<Profile />} />
       <Route path='/battle' element={<Battle />} />
+      <Route path='/chris' element={<Char1 />} />
+      <Route path='/login' element={<Login />} />
 
       
       </Routes>
