b4f4d069724cdcd20b3ec182804f83cb502eccfa
johnn4127 <johnn4127@gmail.com>
2023-11-01 11:50:37 -0400

    moves game components into main components

--- frontend/src/components/Skillaction.jsx
+++ frontend/src/components/Skillaction.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import { PlayerData } from './game/Game'
+import { PlayerData } from './Game'
 import { BattleContext } from './Battle'
 
 const Skillaction = () => {
