41cef2feabae56c8997c03a79dcdbe7148844394
johnn4127 <johnn4127@gmail.com>
2023-11-03 14:24:50 -0400

    update

--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -1,14 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import mp3 from '../assets/music/Registration.mp3';
 import { Form, Button, Alert, Container } from 'react-bootstrap';
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import '../stylesheets/Registration.css'
 function RegisterForm() {
   const audio = new Audio(mp3);
-  
-  
 
-  
+
+
+
 
   const [formData, setFormData] = useState({
     email: '',
@@ -25,10 +25,10 @@ function RegisterForm() {
 
   const handleRegister = async () => {
     if (formData.password !== formData.confirmPassword) {
-     
+
       setRegistrationStatus('passwordMismatch');
       return;
-      
+
     }
 
     if (!formData.email.trim() || !formData.password.trim()) {
@@ -51,7 +51,7 @@ function RegisterForm() {
 
       setRegistrationStatus('emailTaken');
     } else {
- 
+
       setRegistrationStatus('failure');
     }
   };
@@ -62,87 +62,87 @@ function RegisterForm() {
 
   return (
     <div className='mainContainer'>
-      <h1 style={{fontSize:'75px',marginTop:'100px'}} className="text-center">Registration</h1>
-    <Container
-      className="d-flex justify-content-center align-items-start"
-      style={{ Height: '100%' }}
-    >
-      <div  style={{ marginTop: '0px', }}>
-        
-        <Form>
-          <Form.Group  controlId="formBasicEmail">
-            <Form.Label style={{margin:'10px'}}>Email Address</Form.Label>
-            <Form.Control
-              type="email"
-              name="email"
-              placeholder="Enter email"
-              value={formData.email}
-              onChange={handleInputChange}
-            />
-          </Form.Group>
+      <h1 style={{ fontSize: '75px', marginTop: '100px' }} className="text-center">Registration</h1>
+      <Container
+        className="d-flex justify-content-center align-items-start"
+        style={{ Height: '100%' }}
+      >
+        <div style={{ marginTop: '0px', }}>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label style={{margin:'10px'}}>Password</Form.Label>
-            <Form.Control
-              type="password"
-              name="password"
-              placeholder="Password"
-              value={formData.password}
-              onChange={handleInputChange}
-            />
-          </Form.Group>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label style={{ margin: '10px' }}>Email Address</Form.Label>
+              <Form.Control
+                type="email"
+                name="email"
+                placeholder="Enter email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
 
-          <Form.Group controlId="formConfirmPassword">
-            <Form.Label style={{margin:'10px'}}>Confirm Password</Form.Label>
-            <Form.Control
-              type="password"
-              name="confirmPassword"
-              placeholder="Confirm Password"
-              value={formData.confirmPassword}
-              onChange={handleInputChange}
-            />
-          </Form.Group>
-            <div style={{justifyContent:''}}></div>
-          <div style={{ textAlign: 'center', margin: '10px' }}>
-            <Button style={{width:'100px', height:'40px', fontSize:'65%'}} variant="primary" type="button" onClick={handleRegister}>
-              Register
-            </Button>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label style={{ margin: '10px' }}>Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+
+            <Form.Group controlId="formConfirmPassword">
+              <Form.Label style={{ margin: '10px' }}>Confirm Password</Form.Label>
+              <Form.Control
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+                value={formData.confirmPassword}
+                onChange={handleInputChange}
+              />
+            </Form.Group>
+            <div style={{ justifyContent: '' }}></div>
+            <div style={{ textAlign: 'center', margin: '10px' }}>
+              <Button style={{ width: '100px', height: '40px', fontSize: '65%' }} variant="primary" type="button" onClick={handleRegister}>
+                Register
+              </Button>
+            </div>
+            <div style={{ textAlign: 'center', margin: '10px' }}>
+              <Link style={{ textDecoration: 'none', marginTop: '10px' }} to="/login">
+                <a><p style={{ marginTop: '10px', fontSize: '15px', color: 'white' }} >Already Have <br /> an Account?</p></a>
+              </Link>
+            </div>
+          </Form>
+          <div style={{ fontSize: '35%', }}>
+            {registrationStatus === 'success' && (
+              <Alert variant="success" className="mt-3">
+                Registration successful!
+              </Alert>
+            )}
+            {registrationStatus === 'failure' && (
+              <Alert variant="danger" className="mt-3">
+                Registration failed. Please try again.
+              </Alert>
+            )}
+            {registrationStatus === 'passwordMismatch' && (
+              <Alert variant="danger" className="mt-3">
+                Password and confirmation do not match.
+              </Alert>
+            )}
+            {registrationStatus === 'emailPasswordError' && (
+              <Alert variant="danger" className="mt-3">
+                Please enter a valid email address and password.
+              </Alert>
+            )}
+            {registrationStatus === 'emailTaken' && (
+              <Alert variant="danger" className="mt-3">
+                This email is already taken. Please use a different email.
+              </Alert>
+            )}
           </div>
-        <div style={{ textAlign: 'center', margin: '10px' }}>
-        <Link style={{textDecoration:'none', marginTop:'10px'}} to="/login">
-            <a><p style={{marginTop:'10px', fontSize:'15px', color:'white'}} >Already Have <br /> an Account?</p></a>
-            </Link>
         </div>
-        </Form>
-        <div style={{fontSize:'35%',}}>
-        {registrationStatus === 'success' && (
-          <Alert variant="success" className="mt-3">
-            Registration successful!
-          </Alert>
-        )}
-        {registrationStatus === 'failure' && (
-          <Alert variant="danger" className="mt-3">
-            Registration failed. Please try again.
-          </Alert>
-        )}
-        {registrationStatus === 'passwordMismatch' && (
-          <Alert variant="danger" className="mt-3">
-            Password and confirmation do not match.
-          </Alert>
-        )}
-        {registrationStatus === 'emailPasswordError' && (
-          <Alert variant="danger" className="mt-3">
-            Please enter a valid email address and password.
-          </Alert>
-        )}
-        {registrationStatus === 'emailTaken' && (
-          <Alert variant="danger" className="mt-3">
-            This email is already taken. Please use a different email.
-          </Alert>
-        )}
-        </div>
-      </div>
-    </Container>
+      </Container>
     </div>
   );
 }
