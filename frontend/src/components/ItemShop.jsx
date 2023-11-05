5b932abbdee534893bc3c5d91833e209964f427e
Dujuanye' Henry <139405590+dhenry827@users.noreply.github.com>
2023-11-05 00:39:09 -0400

    Shop items (#97)
    * Adds more enemies and staggers enemy positioning. Also adds ability for individual enemy images.
    
    * Adds upgrade shop, sets up conditional rendering for shop type and disables buttons after appropriate use.
    
    * Adds upgrade shop, sets up conditional rendering for shop type and disables buttons after appropriate use.
    
    * Adds upgrade shop, sets up conditional rendering for shop type and disables buttons after appropriate use.
    
    ---------
    
    Co-authored-by: COR Coding <corcoding@coding-14.attlocal.net>

---
+++ frontend/src/components/ItemShop.jsx
@@ -0,0 +1,64 @@
+import React, { useContext, useState } from 'react'
+import { PlayerData } from './Game'
+
+const ItemShop = () => {
+
+    const { player, setPlayer, setShop, setPause } = useContext(PlayerData)
+
+    const [item1, setItem1] = useState({
+        name: 'Armor Upgrade',
+        desc: 'Increases max HP by 50%.',
+        price: 0,
+        uses: 3,
+        effect: () => {
+            if (item1.uses > 0) {
+                setPlayer({ ...player, maxConfidence: player.maxConfidence * 1.5, confidence: player.maxConfidence * 1.5})
+                setItem1({...item1, uses: item1.uses -= 1, price: item1.price *= 1.5})
+                console.log('Bought')
+            }
+        }
+    })
+
+    const [item2, setItem2] = useState({
+        name: 'Damage Amplifier',
+        desc: 'Increases proficency by 50%.',
+        price: 0,
+        uses: 3,
+        effect: () => {
+            if (item2.uses > 0) {
+                setPlayer({ ...player, proficiency: Math.round(player.proficiency * 1.5) })
+                setItem2({...item2, uses: item2.uses -= 1, price: item2.price *= 1.5 })
+            }
+        }
+    })
+
+    const itemShop = [item1, item2]
+
+    const buyUpgrade = (upgrade) => {
+        if (player.exp >= upgrade.price) {
+            upgrade.effect()
+        }
+    }
+
+    return (
+        <>
+        <h3 style={{ alignSelf: 'center' }}>Upgrades</h3>
+        {itemShop.map((item, index) => (
+            <div key={index}>
+                <button
+                    value={item.name}
+                    onClick={() => buyUpgrade(item)}
+                    disabled={item.uses == 0}>
+                    {item.name}
+                    
+                </button>
+                <p>{item.desc}</p>
+                <p>Available: {item.uses}</p>
+                <p>Price: {item.price} exp</p>
+            </div>
+       ))}
+        </>
+    )
+}
+
+export default ItemShop
---
+++ frontend/src/components/SkillShop.jsx
@@ -0,0 +1,102 @@
+import React, { useContext, useState } from 'react'
+import { PlayerData } from './Game'
+
+const SkillShop = () => {
+
+    const { player, setPlayer, setShop, setPause } = useContext(PlayerData)
+
+    const [skill1, setSkill1] = useState({
+        name: 'Take a Breather',
+        description: 'Heals your confidence by 10%.',
+        effect: () => {
+            setPlayer({ ...player, confidence: player.confidence + (Math.round(player.confidence * 0.10)) });
+            setSkill1({...skill1, bought: true})
+            console.log('Bought')
+        },
+        price: 300,
+        bought: false
+    });
+
+    const [skill2, setSkill2] = useState({
+        name: 'Resume Template',
+        description: 'Boosts your proficiency by 15%.',
+        effect: () => {
+            setPlayer({ ...player, proficiency: player.proficiency + (Math.round(player.proficiency * 0.15)) });
+            setSkill2({...skill2, bought: true})
+            console.log('Bought')
+        },
+        price: 200,
+        bought: false
+    });
+
+    const [skill3, setSkill3] = useState({
+        name: 'Star Method',
+        description: 'Heals your confidence by 20%.',
+        effect: () => {
+            setPlayer({ ...player, confidence: player.confidence + (Math.round(player.confidence * 0.20)) });
+            setSkill3({...skill3, bought: true})
+            console.log('Bought')
+        },
+        price: 350,
+        bought: false
+    });
+
+    const [skill4, setSkill4] = useState({
+        name: 'Charm',
+        description: 'Heals your confidence by 10%.',
+        effect: () => {
+            setPlayer({ ...player, confidence: player.confidence + (Math.round(player.confidence * 0.10)) });
+            setSkill4({...skill4, bought: true})
+            console.log('Bought')
+        },
+        price: 200,
+        bought: false
+    });
+
+    const [skill5, setSkill5] = useState({
+        name: 'Concise Elevator Pitch',
+        description: 'Boosts your proficiency by 10%.',
+        effect: () => {
+            setPlayer({ ...player, proficiency: player.proficiency + (Math.round(player.proficiency * 0.10)) });
+            setSkill5({...skill5, bought: true})
+            console.log('Bought')
+        },
+        price: 300,
+        bought: false
+    });
+
+
+    const skillShop = [skill1, skill2, skill3, skill4, skill5]
+
+    const buy = (skill) => {
+        if (player.exp >= skill.price) { //checks if player has enough experience to purchase skill
+            setPlayer({
+                ...player,
+                exp: player.exp - skill.price, //subtracts skill price from players exp points
+                skills: [...player.skills, skill], //adds the purchased skill to the players skills array
+            });
+            
+        }
+
+    }
+
+    return (
+        <>
+            <h3 style={{ alignSelf: 'center' }}>Skills</h3>
+            {skillShop.map((skill, index) => (
+                <div key={index}>
+                    <button
+                        value={skill.name}
+                        onClick={() => buy(skill)}
+                        disabled={skill.bought == true}>
+                        {skill.name}
+                    </button>
+                    <p>{skill.description}</p>
+                    <p>Price: {skill.price} exp</p>
+                </div>
+            ))}
+        </>
+    )
+}
+
+export default SkillShop
