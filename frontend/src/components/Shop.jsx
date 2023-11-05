758732e2720fb60cc4ad7ffe7cb31adf2b0db662
Dujuanye' Henry <139405590+dhenry827@users.noreply.github.com>
2023-11-05 00:44:30 -0400

    Enemy order (#98)
    * Adds more enemies and staggers enemy positioning. Also adds ability for individual enemy images.
    
    * Adds upgrade shop, sets up conditional rendering for shop type and disables buttons after appropriate use.
    
    * Makes it so that enemies must be fought in a specific order.
    
    ---------
    
    Co-authored-by: COR Coding <corcoding@coding-14.attlocal.net>

--- frontend/src/components/Shop.jsx
+++ frontend/src/components/Shop.jsx
@@ -32,6 +32,21 @@ const Shop = () => {
                         {!storeMode ? (<>Upgrades</>) : (<>Skills</>)}
                     </button>
 
+                    <button onClick={handleStoreMode}>
+                        {!storeMode ? (<>Upgrades</>) : (<>Skills</>)}
+                    </button>
+
+                    <div id='shopItems' style={{ overflow: 'scroll' }}>
+                        {!storeMode ? (<SkillShop />) : (<ItemShop />)}
+                        <p>Current EXP: {player.exp}</p>
+                    </div>
+                    <div>
+
+                    </div>
+                    <div id='exit'>
+                        <button style={{ position: "absolute", bottom: 0 }} onClick={() => closeShop()}>Exit Shop</button>
+
+                    </div>
                 </div>
                 <div>
                     
