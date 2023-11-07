11afa007ef8871f9f8ba974fa1f352281953b1f0
John Nguyen <johnn4127@gmail.com>
2023-11-06 22:15:25 -0500

    adds login and register deployed end points (#119)

--- frontend/src/components/Registration.jsx
+++ frontend/src/components/Registration.jsx
@@ -38,7 +38,7 @@ const audio = new Audio(mp3);
       return;
     }
 
-    const response = await fetch('http://localhost:3000/register', {
+    const response = await fetch('https://capstone-project-beige-delta.vercel.app/register', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
