72e05d0692f3541f7f1d5bc679cad81dca70a71a
johnn4127 <johnn4127@gmail.com>
2023-11-06 22:14:39 -0500

    adds login and register deployed end points

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:3000/login', {
+      const response = await fetch('https://capstone-project-beige-delta.vercel.app/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
