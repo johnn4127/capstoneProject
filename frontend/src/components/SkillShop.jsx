ca3bcbea653a988d1d2cf1b9d1876e5aba3c7ee9
Dujuanye' Henry <139405590+dhenry827@users.noreply.github.com>
2023-11-08 15:12:39 -0500

    Changes healing item logic to set player hp to a specific percentage. (#124)
    Co-authored-by: COR Coding <corcoding@DESKTOP-QMOVT9L.localdomain>

--- frontend/src/components/SkillShop.jsx
+++ frontend/src/components/SkillShop.jsx
@@ -8,69 +8,69 @@ const SkillShop = () => {
     //Contexts
     const { player, setPlayer } = useContext(PlayerData)
 
+    
     //States
     const [skill1, setSkill1] = useState({
         name: 'Delicious Apple',
-        description: 'Heals your HP by 110%.',
+        description: 'Sets your HP to 25%.',
         effect: () => {
-            setPlayer({ ...player, confidence: player.confidence + (Math.round(player.confidence * 0.10)) });
-            setSkill1({...skill1, bought: true})
+            setPlayer({ ...player, confidence: Math.round(player.maxConfidence * 0.25) })
+            setSkill1({ ...skill1, bought: true })
             console.log('Bought')
         },
-        price: 300,
+        price: 100,
         bought: false
     });
 
     const [skill2, setSkill2] = useState({
-        name: 'Enery Drink',
-        description: 'Boosts your proficiency by 50%.',
+        name: 'Bandage',
+        description: 'Sets your HP to 50%.',
         effect: () => {
-            setPlayer({ ...player, proficiency: player.proficiency + 100 });
+            setPlayer({ ...player, confidence: Math.round(player.maxConfidence * 0.50)  });
             setSkill2({...skill2, bought: true})
             console.log('Bought')
         },
-        price: 200,
+        price: 150,
         bought: false
     });
-
+    
     const [skill3, setSkill3] = useState({
-        name: 'Med Kit',
-        description: 'Heals your HP by 250.',
+        name: 'Medkit',
+        description: 'Sets your HP to 75%.',
         effect: () => {
-            setPlayer({ ...player, confidence: player.confidence + 250 });
+            setPlayer({ ...player, confidence: Math.round(player.maxConfidence * 0.75) });
             setSkill3({...skill3, bought: true})
             console.log('Bought')
         },
-        price: 350,
-        bought: false
-    });
-
-    const [skill4, setSkill4] = useState({
-        name: 'Bandage',
-        description: 'Heals your HP by 110%.',
-        effect: () => {
-            setPlayer({ ...player, confidence: player.confidence + (Math.round(player.confidence * 0.10)) });
-            setSkill4({...skill4, bought: true})
-            console.log('Bought')
-        },
         price: 200,
         bought: false
     });
-
-    const [skill5, setSkill5] = useState({
-        name: 'Power Up',
-        description: 'Boosts your proficiency by 10%.',
+    
+    const [skill4, setSkill4] = useState({
+        name: 'Nano Stim',
+        description: 'Sets your HP to 100%.',
         effect: () => {
-            setPlayer({ ...player, proficiency: player.proficiency + (Math.round(player.proficiency * 0.10)) });
-            setSkill5({...skill5, bought: true})
+            setPlayer({ ...player, proficiency: player.maxConfidence });
+            setSkill4({...skill4, bought: true})
             console.log('Bought')
         },
         price: 300,
         bought: false
     });
+    
+    // const [skill5, setSkill5] = useState({
+    //     name: 'Enery Drink',
+    //     description: 'Boosts your proficiency by 50%.',
+    //     effect: () => {
+    //         setPlayer({ ...player, proficiency: player.proficiency + 100 });
+    //         setSkill5({...skill5, bought: true})
+    //         console.log('Bought')
+    //     },
+    //     price: 200,
+    //     bought: false
+    // });
 
-
-    const skillShop = [skill1, skill2, skill3, skill4, skill5]
+    const skillShop = [skill1, skill2, skill3, skill4]
 
     const buy = (skill) => {
         if (player.exp >= skill.price) { 
